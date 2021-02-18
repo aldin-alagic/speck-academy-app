5154ba0510c1ea8257ee979d857f210af6c655e3
aalagic <aldin_alagic@hotmail.com>
2021-02-18 21:02:49 +0100

    Add conditional displaying of cart products to the Cart component
    Update Cart component so that it displays the products using the Product component.

--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -1,5 +1,34 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
+
+import { Product } from "../product/Product";
 
 export function Cart() {
-  return <Fragment></Fragment>;
+  const [products, setProducts] = useState([]);
+  const numberOfProducts = products.length;
+
+  const handleRemoveProducts = () => {
+    const removeProducts = window.confirm(
+      "Are you sure you want to remove all products?"
+    );
+    if (removeProducts) setProducts([]);
+  };
+
+  return (
+    <Fragment>
+      {numberOfProducts > 0 && (
+        <div className="d-flex justify-content-between">
+          <p>
+            Number of products:
+            <span className="text-danger"> {numberOfProducts}</span>
+          </p>
+          <button className="remove-button" onClick={handleRemoveProducts}>
+            Remove all products
+          </button>
+        </div>
+      )}
+      {products.map((product, index) => (
+        <Product key={index} name={product} />
+      ))}
+    </Fragment>
+  );
 }
