aab2fa337c13d6a0bfa11128d0120adfbb1d61bf
aalagic <aldin_alagic@hotmail.com>
2021-02-18 21:04:53 +0100

    Add product addition to the Cart component so that user can add products to the cart
    Update Cart component so that user can add products to the cart using state hooks.

--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -2,8 +2,11 @@ import React, { Fragment, useState } from "react";
 
 import { Product } from "../product/Product";
 
+import "./cart.css";
+
 export function Cart() {
   const [products, setProducts] = useState([]);
+  const [product, setProduct] = useState("");
   const numberOfProducts = products.length;
 
   const handleRemoveProducts = () => {
@@ -15,6 +18,31 @@ export function Cart() {
 
   return (
     <Fragment>
+      <div className=" my-5">
+        <label htmlFor="product-title">Product title</label>
+        <div className="d-flex">
+          <input
+            type="text"
+            className="form-control mr-2"
+            id="product-title"
+            name="product-title"
+            value={product}
+            onChange={(e) => {
+              setProduct(e.target.value);
+            }}
+          />
+          <button
+            onClick={() => {
+              setProducts([...products, product]);
+              setProduct("");
+            }}
+            className="btn add-button"
+          >
+            Add product
+          </button>
+        </div>
+      </div>
+
       {numberOfProducts > 0 && (
         <div className="d-flex justify-content-between">
           <p>
