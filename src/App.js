009c1581acb225d60625a5cee59707c47c48a845
aalagic <aldin_alagic@hotmail.com>
2021-02-18 21:05:15 +0100

    Import and use Cart and Heading components in the App component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React, { PureComponent } from "react";
 
+import { Heading } from "./components/Heading";
+import { Cart } from "./components/cart/Cart";
+
 import "./App.css";
 
 export class App extends PureComponent {
   render() {
     return (
       <div className="main-container">
-        <main className="container col-9"></main>
+        <main className="container col-9">
+          <Heading />
+          <Cart />
+        </main>
       </div>
     );
   }
